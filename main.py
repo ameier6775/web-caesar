f7d667b6d95ebf3a2099b19be2e2ebb6e434e0bf
Alex Meier <awmmcb@mail.missouri.edu>
2019-03-30 00:03:29 -0500

    Finish web caesar

--- main.py
+++ main.py
@@ -1,47 +1,61 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-form = <!DOCTYPE html>
-<html>
-            <head>
-                <style>
-                    form {
-                        background-color: #eee;
-                        padding: 20px;
-                        margin: 0 auto;
-                        width: 540px;
-                        font: 16px sans-serif;
-                        border-radius: 10px;
-                    }
-                    textarea {
-                        margin: 10px 0;
-                        width: 540px;
-                        height: 120px;
-                    }
-                </style>
-            </head>
-            <body>
-            <!-- create your form here -->
-            </body>
-        </html>
 
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form method={{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+      <form action="/something" method="post">
+
+        <label for="rot">
+            ENTER #
+            <input type="number" name="rot" value="0"/>
+        </label>
+        <label for="text">
+            <textarea id="text" type=text name="text">
+            {0}
+            </textarea>
+        </label>
+        <input type="submit" value="Submit">
+      </form>
+    </body>
+</html>
+"""
 @app.route("/")
 def index():
+    result = form.format("")
+    return result
+
+@app.route("/something" , methods=['POST'])
+def encrypt():
+    en_num = int(request.form['rot'])
+    en_mes = request.form['text']
     
+    result = rotate_string(en_mes, en_num)
+    message = form.format(result)
 
-
-    firstAnswer = <input type="text" name="rot" 
-        <textarea rows="3" cols="20">
-
-        </textarea>
-    >
-    secondAnswer = <input type="text" name="rot"
-        <textarea rows="3" cols="20">
-
-        </textarea>
-    >
-
-    return result 
+    return '<h1>' + message + '</h1>'
 
 app.run()
