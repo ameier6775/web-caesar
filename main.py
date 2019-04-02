d05b5ccea2538bfa9736501fd8f5bc9f9bb9818b
Alex Meier <awmmcb@mail.missouri.edu>
2019-04-01 19:16:14 -0500

    Changed my CSS up

--- main.py
+++ main.py
@@ -8,47 +8,48 @@ app.config['DEBUG'] = True
 form = """
 <!DOCTYPE html>
 
-<html>
-    <head>
-        <style>
-            form method={{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-    </head>
-    <body>
-      <form action="/something" method="post">
+    <html>
+        <head>
+            <style>
+                form {{
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }}
 
-        <label for="rot">
-            ENTER #
-            <input type="number" name="rot" value="0"/>
-        </label>
-        <label for="text">
-            <textarea id="text" type=text name="text">
-            {0}
-            </textarea>
-        </label>
-        <input type="submit" value="Submit">
-      </form>
-    </body>
-</html>
+                textarea {{
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }}
+            </style>
+        </head>
+        <body>
+        <form action="/" method="post">
+
+            <label for="rot">
+                ENTER #
+                <input type="text" name="rot" value="0"/>
+            </label>
+            <label for="text">
+                <textarea id="text" type="text" name="text">
+                {0}
+                </textarea>
+            </label>
+            <input type="submit" value="Submit">
+        </form>
+        </body>
+    </html>
 """
 @app.route("/")
 def index():
     result = form.format("")
     return result
 
-@app.route("/something" , methods=['POST'])
+@app.route("/", methods=["POST"])
 def encrypt():
     en_num = int(request.form['rot'])
     en_mes = request.form['text']
